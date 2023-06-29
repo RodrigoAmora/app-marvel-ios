b4b722542c645bea9970f35f87045bfcb4e05038
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-29 10:34:29 -0300

    Criando a classe Character

---
+++ Marvel App/Model/Character.swift
@@ -0,0 +1,30 @@
+//
+//  Character.swift
+//  Marvel App
+//
+//  Created by Rodrigo Amora on 29/06/23.
+//
+
+import Foundation
+import CoreData
+import ObjectMapper
+
+class Character: NSObject {
+    var id: Int = 0
+    var name: String = ""
+    var characterDescription: String = ""
+    
+    required init?(map: Map) {}
+    
+}
+
+// MARK: Mappable
+extension Character: Mappable {
+    
+    func mapping(map: Map) {
+        id          <- map["id"]
+        name        <- map["name"]
+        characterDescription    <- map["description"]
+    }
+    
+}
