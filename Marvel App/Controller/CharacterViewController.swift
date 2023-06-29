c1e7e7b1bf59f51ece4f349616b2a20d26fc3693
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-29 10:39:48 -0300

    Criando a tela de lista de personagens

---
+++ Marvel App/Controller/CharacterViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterViewController.swift
+//  Marvel App
+//
+//  Created by Rodrigo Amora on 29/06/23.
+//
+
+import Foundation
+
+class CharacterViewController: ViewController {
+    
+}
