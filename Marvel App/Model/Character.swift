ceb765ba9c2c3d038e495938097718ef6145aa59
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-30 06:02:57 -0300

    Mapeando os campos do Json

--- Marvel App/Model/Character.swift
+++ Marvel App/Model/Character.swift
@@ -7,26 +7,33 @@
 
 import Foundation
 import CoreData
-import ObjectMapper
 
-class Character: NSObject, Decodable {
+class Character: NSObject, NSCoding, Decodable {
+    
     
     // MARK: - Atributes
     var id: Int = 0
     var name: String = ""
     var characterDescription: String = ""
     
-    required init?(map: Map) {}
+    // MARK: - NSCoding
+    func encode(with coder: NSCoder) {
+        coder.encode(id, forKey: "id")
+        coder.encode(name, forKey: "name")
+        coder.encode(characterDescription, forKey: "description")
+    }
     
-}
-
-// MARK: Mappable
-extension Character: Mappable {
+    required init?(coder: NSCoder) {
+        id = coder.decodeObject(forKey: "id") as! Int
+        name = coder.decodeObject(forKey: "name") as! String
+        characterDescription = coder.decodeObject(forKey: "description") as! String
+    }
     
-    func mapping(map: Map) {
-        id          <- map["id"]
-        name        <- map["name"]
-        characterDescription    <- map["description"]
+    // MARK: - CodingKeys
+    enum CodingKeys: String, CodingKey {
+        case id
+        case characterDescription = "description"
+        case name
     }
     
 }
