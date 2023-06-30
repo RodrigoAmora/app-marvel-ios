e27d6d54a093f5923a05beb27138a042fc8e5be1
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-30 10:21:10 -0300

    Criando a classe Thumbnail

--- Marvel App/Model/Character.swift
+++ Marvel App/Model/Character.swift
@@ -10,23 +10,25 @@ import CoreData
 
 class Character: NSObject, NSCoding, Decodable {
     
-    
     // MARK: - Atributes
     var id: Int = 0
     var name: String = ""
     var characterDescription: String = ""
+    var thumbnail: Thumbnail?
     
     // MARK: - NSCoding
     func encode(with coder: NSCoder) {
         coder.encode(id, forKey: "id")
         coder.encode(name, forKey: "name")
         coder.encode(characterDescription, forKey: "description")
+        coder.encode(thumbnail, forKey: "thumbnail")
     }
     
     required init?(coder: NSCoder) {
         id = coder.decodeObject(forKey: "id") as! Int
         name = coder.decodeObject(forKey: "name") as! String
         characterDescription = coder.decodeObject(forKey: "description") as! String
+        thumbnail = coder.decodeObject(forKey: "thumbnail") as! Thumbnail
     }
     
     // MARK: - CodingKeys
@@ -34,6 +36,7 @@ class Character: NSObject, NSCoding, Decodable {
         case id
         case characterDescription = "description"
         case name
+        case thumbnail
     }
     
 }
---
+++ Marvel App/Model/Thumbnail.swift
@@ -0,0 +1,33 @@
+//
+//  Thumbnail.swift
+//  Marvel App
+//
+//  Created by Rodrigo Amora on 30/06/23.
+//
+
+import Foundation
+
+class Thumbnail: NSObject, NSCoding, Decodable {
+    
+    // MARK: - Atributes
+    var extensionPhoto: String
+    var path: String
+    
+    // MARK: - NSCoding
+    func encode(with coder: NSCoder) {
+        coder.encode(extensionPhoto, forKey: "extensionPhoto")
+        coder.encode(path, forKey: "path")
+    }
+    
+    required init?(coder: NSCoder) {
+        extensionPhoto = coder.decodeObject(forKey: "extensionPhoto") as! String
+        path = coder.decodeObject(forKey: "path") as! String
+    }
+    
+    // MARK: - CodingKeys
+    enum CodingKeys: String, CodingKey {
+        case extensionPhoto = "extension"
+        case path
+    }
+    
+}
