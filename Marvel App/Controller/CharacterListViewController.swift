145312d64085591786360bf66521d38d4150f1e7
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-08 09:57:12 -0300

    Refatorando os métodos de busca de personagens

--- Marvel App/Controller/CharacterListViewController.swift
+++ Marvel App/Controller/CharacterListViewController.swift
@@ -18,11 +18,9 @@ class CharacterListViewController : BaseViewController {
     
     // MARK: - Atributes
     private var characters: [Character] = []
-    private lazy var characterViewModel: CharacterViewModel = CharacterViewModel(characterProtocol: self)
+    private lazy var characterViewModel: CharacterViewModel = CharacterViewModel(characterDelegate: self)
     private var fab: MDCFloatingButton!
     private let refreshControl = UIRefreshControl()
-    
-    
     private var offset = 0
     
     // MARK: - View life cycle
@@ -73,7 +71,7 @@ class CharacterListViewController : BaseViewController {
     }
     
     private func getCharacters() {
-        characterViewModel.getCharacters(offset: offset, completion: { resource in })
+        characterViewModel.getCharacters(offset: offset)
     }
     
     @objc private func paginateTableView() {
@@ -82,9 +80,8 @@ class CharacterListViewController : BaseViewController {
         //refreshControl.endRefreshing()
     }
     
-    private func getCharactersByName(name: String) {
-        let nameCharacter = searchBar?.text?.replacingOccurrences(of: " ", with: "%20") ?? ""
-        characterViewModel.getCharactersByName(name: nameCharacter, completion: { [weak self] resource in})
+    private func getCharactersByName(_ name: String) {
+        characterViewModel.getCharactersByName(name)
     }
     
     @objc func showSearchView() {
@@ -115,10 +112,13 @@ extension CharacterListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row == characters.count-1 {
+        if indexPath.row == characters.count-1, characters.count > 20 {
             paginateTableView()
-            let targetRonIdexPath = IndexPath(row: indexPath.row-5, section: 0)
-            characterTableView.scrollToRow(at: targetRonIdexPath, at: .middle, animated: false)
+            
+//            if indexPath.row > 20 {
+//                let targetRonIdexPath = IndexPath(row: indexPath.row-5, section: 0)
+//                characterTableView.scrollToRow(at: targetRonIdexPath, at: .middle, animated: false)
+//            }
         }
     }
 }
@@ -141,8 +141,8 @@ extension CharacterListViewController: UITableViewDelegate {
 // MARK: - UISearchBarDelegate
 extension CharacterListViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        let nameCharacter = searchBar.text ?? ""
-        getCharactersByName(name: nameCharacter)
+        let nameCharacter = searchBar.text?.replacingOccurrences(of: " ", with: "%20") ?? ""
+        getCharactersByName(nameCharacter)
     }
 }
 
@@ -151,7 +151,23 @@ extension CharacterListViewController: CharacterDelegaate {
     func populateTableView(characters: [Character]) {
         self.characters = characters
         self.characterTableView.reloadData()
+        
+        if offset != 0 {
+            let targetRonIdexPath = IndexPath(row: self.characters.count-5, section: 0)
+            characterTableView.scrollToRow(at: targetRonIdexPath, at: .middle, animated: false)
+        }
+    }
+    
+    func replaceAll(characters: [Character]) {
+        self.characters.removeAll()
+        self.characters = characters
+        self.characterTableView.reloadData()
         self.searchBar.isHidden = true
+        
+        if characters.count > 10 {
+            let targetRonIdexPath = IndexPath(row: 0, section: 0)
+            characterTableView.scrollToRow(at: targetRonIdexPath, at: .top, animated: false)
+        }
     }
     
     func showError(_ errorCode: Int) {
