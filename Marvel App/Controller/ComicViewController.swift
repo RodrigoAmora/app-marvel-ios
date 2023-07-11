a1136f18e06dc2f40ccf1f8cc05d8958ad0e3e66
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-11 02:36:42 -0300

    Criando o ComicViewController

---
+++ Marvel App/Controller/ComicViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ComicViewController.swift
+//  Marvel App
+//
+//  Created by Rodrigo Amora on 10/07/23.
+//
+
+import Foundation
+import UIKit
+
+class ComicViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageComicImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var comicLabel: UILabel!
+    @IBOutlet weak var pickerComics: UIPickerView!
+    
+    // MARK: - Atributes
+    private var comic: Comic?
+    
+    // MARK: - init
+    class func intanciate(_ comic: Comic) -> ComicViewController {
+        let controller = ComicViewController()
+        controller.comic = comic
+        
+        return controller
+    }
+    
+    // MARK: - View life cycle
+    override func viewDidLoad() {
+        self.configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        self.navigationItem.title = String(localized: "app_name")
+        
+        self.navigationController?.navigationBar.backgroundColor = .blue
+        self.navigationController?.navigationBar.tintColor = .cyan
+        self.navigationController?.navigationBar.topItem?.title = String(localized: "back")
+    }
+    
+    private func initViews() {
+        let photoComicURL = "\(comic?.thumbnail?.path).\(comic?.thumbnail?.extensionPhoto)"
+        imageComicImageView.loadImageFromURL(photoComicURL)
+        
+        titleLabel.textAlignment = .center
+        titleLabel.text = comic?.title
+        
+        if ((comic?.description.isEmpty) != nil) {
+            descriptionLabel.text = String(localized: "character_no_description")
+        } else {
+            descriptionLabel.textColor = .black
+            descriptionLabel.text = comic?.comicDescription
+        }
+    }
+}
