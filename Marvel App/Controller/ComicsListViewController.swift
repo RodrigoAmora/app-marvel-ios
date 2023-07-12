587cc6a9c5ffe52f4f7dc28a4f3b55d16ebc2551
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-12 02:25:19 -0300

    Criando a tela lista de HQs

---
+++ Marvel App/Controller/ComicsListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ComicsListViewController.swift
+//  Marvel App
+//
+//  Created by Rodrigo Amora on 12/07/23.
+//
+
+import Foundation
+import UIKit
+
+class ComicsListViewController: BaseViewController {
+    
+}
