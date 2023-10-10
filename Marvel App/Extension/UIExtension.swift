3beee694089c294c73ba6146f580a52158c435ba
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-10-10 01:26:33 -0300

    Criando a extension de  UIActivityIndicatorView

--- Marvel App/Extension/UIExtension.swift
+++ Marvel App/Extension/UIExtension.swift
@@ -8,6 +8,19 @@
 import Foundation
 import UIKit
 
+extension UIActivityIndicatorView {
+    func configureActivityIndicatorView() {
+        self.backgroundColor = .gray
+        self.layer.cornerRadius = CGRectGetWidth(self.frame)/4.0
+        self.color = .blue
+        self.isHidden = false
+    }
+    
+    func hideActivityIndicatorView() {
+        self.isHidden = true
+    }
+}
+
 extension UIImageView {
     func loadImageFromURL(_ url: String) {
         guard let imageURL = URL(string: url) else { return }
