df88da835c46e184aae31a4a710924061fa9999c
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-12-11 04:22:52 -0300

    Adicionando marcadores

--- Marvel AppUITests/CharacterUITests.swift
+++ Marvel AppUITests/CharacterUITests.swift
@@ -9,13 +9,15 @@ import XCTest
 
 final class CharacterUITests: XCTestCase {
     
+    // MARK: - Atributes
     var app: XCUIApplication!
 
+    // MARK: - setUp
     override func setUp() {
         continueAfterFailure = false
-        app = XCUIApplication()
-        app.launchArguments = ["testing"]
-        app.launch()
+        self.app = XCUIApplication()
+        self.app.launchArguments = ["testing"]
+        self.app.launch()
     }
     
     override func setUpWithError() throws {
@@ -24,22 +26,6 @@ final class CharacterUITests: XCTestCase {
 
     override func tearDownWithError() throws {}
 
-    func testVerifiyNameOfFirstCharacter() throws {
-        let characterTableView = app.tables
-        XCTAssertEqual("3-D Man", characterTableView.cells.element(boundBy: 0).staticTexts["nameCharacter"].label)
-    }
-
-    func testVerifiyNameAndDescriptionOfCharacterAfterSelectOneCharacter() throws {
-        let characterTableView = app.tables
-        characterTableView.cells.element(boundBy: 0).firstMatch.tap()
-        
-        let nameCharacterLabel = app.staticTexts["nameCharacter"]
-        let descriptionCharacterLabel = app.staticTexts["descriptionCharacter"]
-        
-        XCTAssertEqual("3-D Man", nameCharacterLabel.label)
-        XCTAssertNotNil(descriptionCharacterLabel.label)
-    }
-    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
@@ -48,4 +34,22 @@ final class CharacterUITests: XCTestCase {
             }
         }
     }
+    
+    // MARK: - Tests
+    func testVerifiyNameOfFirstCharacter() throws {
+        let characterTableView = self.app.tables
+        XCTAssertEqual("3-D Man", characterTableView.cells.element(boundBy: 0).staticTexts["nameCharacter"].label)
+    }
+
+    func testVerifiyNameAndDescriptionOfCharacterAfterSelectOneCharacter() throws {
+        let characterTableView = self.app.tables
+        characterTableView.cells.element(boundBy: 0).firstMatch.tap()
+        
+        let nameCharacterLabel = self.app.staticTexts["nameCharacter"]
+        let descriptionCharacterLabel = self.app.staticTexts["descriptionCharacter"]
+        
+        XCTAssertEqual("3-D Man", nameCharacterLabel.label)
+        XCTAssertNotNil(descriptionCharacterLabel.label)
+    }
+    
 }
